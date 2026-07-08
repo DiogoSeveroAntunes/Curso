8e9f7981749f3abd453e18afc28eb0ce1b4d7c4b
DiogoSeveroAntunes <tchais777@hotmail.com>
2026-07-07 21:04:08 -0300

    colocando comentários no código

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Pessoa p = new Pessoa();
+    // metodo para pegar a informação digitada no teclado e salva-la para uso futuro//
     Scanner s = new Scanner(System.in);
     System.out.println("Digite o Seu nome");
+    //pegando a informação digitada pelo usuário
     String nomeDigitado = s.nextLine();
+    //colocando a informação e a repassando
     p.setNome(nomeDigitado);
     System.out.println(p.getNome());
+    //código para fechar o scanner
     s.close();
     }
 }
